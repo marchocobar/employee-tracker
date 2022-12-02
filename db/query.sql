63740890a00ca4b55ec92d3d08288096b0e64144
Marissa Baron <mariluvv23@gmail.com>
2022-12-02 14:24:43 -0500

    added queries

---
+++ db/query.sql
@@ -0,0 +1,27 @@
+SELECT * 
+FROM department;
+
+SELECT 
+    role.id, 
+    role.title,
+    role.salary,
+    department.name AS department 
+FROM role
+INNER JOIN department
+    ON role.department_id = department.id;
+
+SELECT
+    employee.id,
+    employee.first_name,
+    employee.last_name,
+    role.title,
+    department.name,
+    role.salary,
+    CONCAT(employee.first_name, ' ', employee.last_name) AS manager,
+FROM employee
+    LEFT JOIN role
+        ON employee.role_id = role.id
+    LEFT JOIN department
+        ON role.department_id = department_id
+    LEFT JOIN employee Manager
+        ON employee.manager_id = Manager.id;
