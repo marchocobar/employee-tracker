3b8d58820b9f31dfb872a0da68ddc0557a0c28a9
Marissa Baron <mariluvv23@gmail.com>
2022-12-02 09:30:09 -0500

    initial files and npm packages

---
+++ server.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'selinakyle',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+  const viewOptions = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add department',
+                'Add role',
+                'Add employee',
+                'Update all departments',
+                'Update employee infomation',
+                'Exit'
+            ]
+        }
+    ])
+    .then(answers)
+}
+  
